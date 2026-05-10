42a76fff7a25b9dbfb6e5f20d4640262ad0559ff
haikal-devtech <haikalpasha207@gmail.com>
2026-05-10 15:11:12 +0700

    Update page.tsx

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -72,7 +72,7 @@ export default function LandingPage() {
             playsInline 
             className="w-full h-full object-cover scale-110"
           >
-            <source src="https://www.pexels.com/download/video/26893760/" type="video/mp4" />
+            <source src="public/videos/pesawat.WEBM" type="video/WEBM" />
           </video>
           <div className="absolute inset-0 bg-gradient-to-b from-slate-900/60 via-slate-900/40 to-[#fafafa]"></div>
         </motion.div>
