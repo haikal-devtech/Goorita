26043a052fb1c455311ffed27585c1e183b51042
haikal-devtech <haikalpasha207@gmail.com>
2026-05-10 17:05:29 +0700

    Delete check_cdn.js

--- check_cdn.js
+++
@@ -1,9 +0,0 @@
-const https = require('https');
-
-const url = 'https://cdn.jsdelivr.net/gh/haikal-devtech/Goorita@main/public/hero-video.mp4';
-
-https.get(url, (response) => {
-  console.log('Status code:', response.statusCode);
-  console.log('Content-Type:', response.headers['content-type']);
-  console.log('Content-Length:', response.headers['content-length']);
-});
